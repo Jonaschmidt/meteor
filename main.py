2d9b4f6d5e9bdd456bceabfd5d8618301041d652
Jonas Schmidt <85327109+Jonaschmidt@users.noreply.github.com>
2022-06-26 11:51:27 -0500

    Implemented Player class
    Implemented Player class and a movement function of that class. Some other minor tweaks, and some updates to files of project_notes.

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ created by Jonas Schmidt on 6/26/2022
 
 import random
 import pygame
+from pygame.locals import *
 
 # TODO: find a good screen ratio
 SCREEN_WIDTH = 270
@@ -15,9 +16,9 @@ TICK_SPEED = 32
 clock = pygame.time.Clock()
 screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT), 0, 32)
 
-# TODO: get this value from the file automatically as opposed to assigning it manually
+# TODO: get these values from the file automatically as opposed to assigning it manually
 meteorSpriteSize = 32
-
+playerSpriteSize = 32
 
 def handle_quit():
     for event in pygame.event.get():
@@ -58,10 +59,39 @@ class Meteor:
                            (self.pos[0] + shadowRadius, self.pos[1] + shadowRadius),
                            shadowRadius)
 
+class Player:
+    global playerSprite
+    playerSprite = pygame.image.load("sprites/basic_front.png")
+
+    def __init__(self):
+        # starting position is in the center of the screen
+        self.pos = ((SCREEN_WIDTH / 2) - (playerSpriteSize / 2), (SCREEN_HEIGHT / 2) - (playerSpriteSize / 2))
+
+    def move(self):
+        pos = list(self.pos)
+
+        # vertical movement
+        if pygame.key.get_pressed()[K_w] and self.pos[1] > 0:
+            pos[1] = pos[1] - 3
+
+        if pygame.key.get_pressed()[K_s] and self.pos[1] < SCREEN_HEIGHT - playerSpriteSize:
+            pos[1] = pos[1] + 3
+
+        # horizontal movement
+        if pygame.key.get_pressed()[K_a] and self.pos[0] > 0:
+            pos[0] = pos[0] - 3
+
+        if pygame.key.get_pressed()[K_d] and self.pos[0] < SCREEN_WIDTH - playerSpriteSize:
+            pos[0] = pos[0] + 3
+
+        self.pos = tuple(pos)
+
+
 def main():
     pygame.init()
 
     meteor = Meteor()
+    player = Player()
 
     while handle_quit():
         clock.tick(TICK_SPEED)
@@ -77,7 +107,13 @@ def main():
         meteor.shadowUpdate(screen)
 
         for c in craterList:
+            # turn this into a function
             screen.blit(meteorSprite, c.pos)
+
+        player.move()
+
+        # turn this into a function
+        screen.blit(playerSprite, player.pos)
 
 
 main()
